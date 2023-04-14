07ed4b60be38ecf0c74ce54a964bd124e4f76dba
Daniel Pollak <pollak@anecdotes.ai>
2023-04-14 15:25:54 +0300

    fix bug

--- ps2.r
+++ ps2.r
@@ -1,5 +1,6 @@
 library(dplyr)
 library(plm)
+library(haven)
 
 simulate_data <- function(
     individuals, periods, alpha, beta, rho_u, rho_tau, tau_vec, gamma = 0
@@ -62,9 +63,22 @@ monte_carlo <- function(
 
         pdat <- pdata.frame(dat, index = c("id", "year"))
 
+        ## First Difference
+        fd_reg1 <- plm(y ~ x, data = pdat, model = "fd")
+        results$fd_beta[r] <- fd_reg1$coefficients["x"]
+
+        # No Clustered SE
+        results$fd_no_corr[r] <-
+            abs(summary(fd_reg1)$coefficients[6]) >= critical_value
+
+        # Clustered SE
+        rob_vcov2 <- vcovHC(fd_reg1, type = "sss", cluster = "group")
+        results$fd_corr[r] <-
+            abs(summary(fd_reg1, rob_vcov2)$coefficients[6]) >= critical_value
+
         ## Fixed Effects
         fe_reg1 <- plm(
-            y ~ x, data = dat, model = "within", effect = "individual"
+            y ~ x, data = pdat, model = "within", effect = "individual"
         )
         results$fe_beta[r] <- fe_reg1$coefficients["x"]
 
@@ -76,19 +90,6 @@ monte_carlo <- function(
         rob_vcov1 <- vcovHC(fe_reg1, type = "sss", cluster = "group")
         results$fe_corr[r] <-
             abs(summary(fe_reg1, rob_vcov1)$coefficients[3]) >= critical_value
-
-        ## First Difference
-        fd_reg1 <- plm(y ~ x, data = pdat, model = "fd")
-        results$fd_beta[r] <- fd_reg1$coefficients["x"]
-
-        # No Clustered SE
-        results$fd_no_corr[r] <-
-            abs(summary(fd_reg1)$coefficients[3]) >= critical_value
-
-        # Clustered SE
-        rob_vcov2 <- vcovHC(fd_reg1, type = "sss", cluster = "group")
-        results$fd_corr[r] <-
-            abs(summary(fd_reg1, rob_vcov2)$coefficients[3]) >= critical_value
     }
 
     return(results)
@@ -120,3 +121,26 @@ r4 <- monte_carlo(250, 20, 5, 0.5, 0.9, 0.02, 200, 1.96)
 # Part B, (3)
 r5 <- monte_carlo(250, 20, 5, 0.5, 0, 0.02, 200, 1.96, 0.05)
 r6 <- monte_carlo(250, 20, 5, 0.5, 0.9, 0.02, 200, 1.96, 0.05)
+
+
+####### Question 2
+
+immg_data <- read_dta("data/immigration8994.dta")
+
+reg_formula <- lnhwage ~ imm3 + sch + exp +
+                expsq + married + nonjew + for_born + timeisr +
+                year90  + year91 + year92 + year93 + year94
+
+immg_pdata <- pdata.frame(immg_data, index = c("occ2", "year"))
+immg_pooled_reg <- lm(reg_formula, data = immg_data) # beta = -0.2083 (0.182)
+
+immg_fe_reg <- plm(reg_formula, data = immg_pdata,
+                    model = "within", effect = "individual") # beta = -0.29550569 (0.10204864) cl - 0.10665030
+
+immg_fd_reg <- plm(reg_formula, data = immg_pdata, model = "fd") # beta = 0.046 (0.18408314) cl - 0.28318235
+
+immg_fe_vcov <- vcovHC(immg_fe_reg, type = "sss", cluster = "group")
+immg_fd_vcov <- vcovHC(immg_fd_reg, type = "sss", cluster = "group")
+
+## Serial correlation test
+pwfdtest(immg_fd_reg, h0 = "fe")
