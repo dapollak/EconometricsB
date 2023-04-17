002d010fe08bfd1cd132c082a070ae021e094bb0
Daniel Pollak <pollak@anecdotes.ai>
2023-04-17 18:40:15 +0300

    add calcs

--- ps2.r
+++ ps2.r
@@ -1,6 +1,7 @@
 library(dplyr)
 library(plm)
 library(haven)
+options(scipen=999)
 
 simulate_data <- function(
     individuals, periods, alpha, beta, rho_u, rho_tau, tau_vec, gamma = 0
@@ -112,7 +113,24 @@ rmse_variation <- function(beta, beta_vector) {
 
 # (2 + 3)
 r1 <- monte_carlo(250, 20, 5, 0, 0, 0.02, 200, 1.96)
+print(c(
+    (rmse_bias(0, r1$fe_beta))^2 + rmse_variation(0, r1$fe_beta),
+    rmse_calc(0, r1$fe_beta)
+))
+print(c(
+    (rmse_bias(0, r1$fd_beta))^2 + rmse_variation(0, r1$fd_beta),
+    rmse_calc(0, r1$fd_beta)
+))
+
 r2 <- monte_carlo(250, 20, 5, 0, 0.9, 0.02, 200, 1.96)
+print(c(
+    (rmse_bias(0, r2$fe_beta))^2 + rmse_variation(0, r2$fe_beta),
+    rmse_calc(0, r2$fe_beta)
+))
+print(c(
+    (rmse_bias(0, r2$fd_beta))^2 + rmse_variation(0, r2$fd_beta),
+    rmse_calc(0, r2$fd_beta)
+))
 
 # (4)
 r3 <- monte_carlo(250, 20, 5, 0.5, 0, 0.02, 200, 1.96)
