fb5bff70ea5889dfec9e49e512ad0d0da5c4ee35
Daniel Pollak <pollak@anecdotes.ai>
2023-07-04 14:35:30 +0300

    .

--- final_work/playground.r
+++ final_work/playground.r
@@ -8,4 +8,3 @@ sf_mortality_race_data <- read_dta("final_work/data/datasets/20080229_SFmortalit
 state_data <- read_dta("final_work/data/datasets/20080229_state_data.dta")
 state_race_data <- read_dta("final_work/data/datasets/20080229_state_race_data.dta")
 national_data <- read_dta("final_work/data/datasets/20080229_national_data.dta")
-    
