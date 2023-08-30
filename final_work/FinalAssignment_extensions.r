a8c7ee2de7c1851fe2efa88ed5ae193c0bbd4acc
Daniel Pollak <pollak@anecdotes.ai>
2023-08-31 00:58:42 +0300

    wip extensions

--- final_work/FinalAssignment_extensions.r
+++ final_work/FinalAssignment_extensions.r
@@ -35,129 +35,13 @@ library(rgdal)
 library(sf)
 library(leaflet)
 library(writexl)
-
+library(car)
+library(pwrss)
 
 
 setwd("G:/האחסון שלי/לימודים/אקונומטריקה/אקונומטריקה ב/פרויקט גמר/AEJApp_Datasets")
 
-### Sub section- State Heterogeneity
-
-## Figure 3 and Table A.1 in the appendix- Treatment effects by state
-e_all <- c()
-e_significant <- c()
-needed_sample <- c()
-se_vals <- c()
-p_vals <- c()
-for (state_s in (subset_state_data_1925_1943_long %>% select(state) %>% distinct())$state) {
-  filtered_data <- filter(
-    subset_state_data_1925_1943_long,
-    state == state_s & disease %in% c("mmr", "tb_rate")
-  )
-
-  reg <- lm(lnm_rate ~ treated:post37 + treated:year_c + treated +
-  year_c + post37, data = filtered_data)
-
-  lh <- linearHypothesis(reg, c(
-    "treatedTRUE:post37TRUE=0"
-  ), white.adjust = "hc1")
-
-  effect <- reg$coefficients["treatedTRUE:post37TRUE"]
-
-  # power analysis
-  p <- pwrss.t.reg(
-    beta1 = effect,
-    # sdy = sd(filtered_data$lnm_rate, na.rm = T),
-    sdx = sqrt(0.5 * (1 - 0.5)), #summary(reg)$coefficients[5, 2]^2,
-    k = 5,
-    r2 = 0.5,
-    power = .7,
-    alpha = 0.05,
-    alternative = "not equal"
-  )
-
-  needed_sample <- c(needed_sample, p$n)
-
-  # store effect and whether significant
-  e_all <- c(e_all, effect)
-  if (lh$"Pr(>F)"[2] < 0.05) {
-    e_significant <- c(e_significant, effect)
-  } else {
-    e_significant <- c(e_significant, 0)
-  }
-
-  se_vals <- c(se_vals, summary(reg)$coefficients["treatedTRUE:post37TRUE","Std. Error"])
-  p_vals <- c(p_vals, summary(reg)$coefficients["treatedTRUE:post37TRUE","Pr(>|t|)"])
-}
-states_data$seperate_effects_all <- e_all
-states_data$seperate_effects_significant <- e_significant
-states_data$power_sample_size <- needed_sample
-states_data$se <- se_vals
-states_data$p_vals <- p_vals
-rownames(states_data) <- states_data$state
-
-# States Effect (Table A.1)
-states_effects <- states_data[c(
-  "seperate_effects_all",
-  "seperate_effects_significant",
-  "se",
-  "p_vals"
-)]
-writeLines(xtable(states_effects), "table_did_weighted.tex")
-
-# dump csv for the heatmap (figure 3)
-write.csv(states_data, "heatmap_mmr.csv")
-
-## Table 6- Extreme State Effects
-extreme_states_effects <- states_effects[
-  c(
-    which.max(states_effects$seperate_effects_all),
-    which.min(states_effects$seperate_effects_all)
-  ),
-][c("seperate_effects_all")]
-print(xtable(extreme_states_effects))
-
-
-## Figure 4 - Black Population Ratio
-#The data is from census 1930 and attached in excel file. Heat maps produced using the website: http://www.heatmapper.ca/geomap/ 
-
-
-## Figure 5
-#from website: https://en.wikipedia.org/wiki/75th_United_States_Congress
-
-
-### Sub section- Diff-in-Diff Model 
-
-## Table A.2 in the appendix- Trend break year by states
-subset_state_data_1925_1943_long$break_output <- subset_state_data_1925_1943_long$lnm_rate - dplyr::lag(subset_state_data_1925_1943_long$lnm_rate)
-
-states_trend_break_full <- data.frame()
-for (state_s in subset_state_data_1925_1943_long$state[!duplicated(subset_state_data_1925_1943_long$state)]) {
-  for (tau in 1933:1940) {
-    mmr_break_reg <- lm(
-      break_output ~ I(year >= tau),
-      data = filter(subset_state_data_1925_1943_long, state == state_s & disease == "mmr")
-    )
-    tryCatch(
-      {
-        mmr_t_val <- coeftest(mmr_break_reg, vcov.=NeweyWest(mmr_break_reg, lag=1, adjust=T))[6]
-
-        states_trend_break_full <- rbind(
-          states_trend_break_full,
-          data.frame(state=state_s, year=tau, f_stat=mmr_t_val^2)
-        )
-      },
-      error = function(e) { }
-    )
-  }
-}
-
-states_trend_break_max <- states_trend_break_full %>%
-            group_by(state) %>%
-            mutate(f_stat_max=max(f_stat)) %>%
-            filter(f_stat == f_stat_max)
-
-states_trend_break_max <- subset(states_trend_break_max, select = -c(f_stat_max))
-writeLines(xtable(states_trend_break_max), "states_year_break.tex")
+### Sub section- Diff-in-Diff Model
 
 ## Figure 6 -8 + Table A.3 - Event study
 
@@ -262,10 +146,128 @@ latex_code <- texreg(table_did_weighted)
 writeLines(latex_code, "table_did_weighted.tex")
 
 
+## Table A.2 in the appendix- Trend break year by states
+merged_state_data$break_output <- merged_state_data$lnm_rate - dplyr::lag(merged_state_data$lnm_rate)
+
+states_trend_break_full <- data.frame()
+for (state_s in merged_state_data$state[!duplicated(merged_state_data$state)]) {
+  for (tau in 1933:1940) {
+    mmr_break_reg <- lm(
+      break_output ~ I(year >= tau),
+      data = filter(merged_state_data, state == state_s & disease == "mmr")
+    )
+    tryCatch(
+      {
+        mmr_t_val <- coeftest(mmr_break_reg, vcov.=NeweyWest(mmr_break_reg, lag=1, adjust=T))[6]
+
+        states_trend_break_full <- rbind(
+          states_trend_break_full,
+          data.frame(state=state_s, year=tau, f_stat=mmr_t_val^2)
+        )
+      },
+      error = function(e) { }
+    )
+  }
+}
+
+states_trend_break_max <- states_trend_break_full %>%
+            group_by(state) %>%
+            mutate(f_stat_max=max(f_stat)) %>%
+            filter(f_stat == f_stat_max)
+
+states_trend_break_max <- subset(states_trend_break_max, select = -c(f_stat_max))
+writeLines(print(xtable(states_trend_break_max)), "states_year_break.tex")
+
+### Sub section- State Heterogeneity
+
+## Figure 3 and Table A.1 in the appendix- Treatment effects by state
+e_all <- c()
+e_significant <- c()
+needed_sample <- c()
+se_vals <- c()
+p_vals <- c()
+for (state_s in (merged_state_data %>% select(state) %>% distinct())$state) {
+  filtered_data <- filter(
+    merged_state_data,
+    state == state_s & disease %in% c("mmr", "tb_rate")
+  )
+
+  reg <- lm(lnm_rate ~ treated:post37 + treated:year_c + treated +
+  year_c + post37, data = filtered_data)
+
+  lh <- linearHypothesis(reg, c(
+    "treatedTRUE:post37TRUE=0"
+  ), white.adjust = "hc1")
+
+  effect <- reg$coefficients["treatedTRUE:post37TRUE"]
+
+  # power analysis
+  p <- pwrss.t.reg(
+    beta1 = effect,
+    # sdy = sd(filtered_data$lnm_rate, na.rm = T),
+    sdx = sqrt(0.5 * (1 - 0.5)), #summary(reg)$coefficients[5, 2]^2,
+    k = 5,
+    r2 = 0.5,
+    power = .7,
+    alpha = 0.05,
+    alternative = "not equal"
+  )
+
+  needed_sample <- c(needed_sample, p$n)
+
+  # store effect and whether significant
+  e_all <- c(e_all, effect)
+  if (lh$"Pr(>F)"[2] < 0.05) {
+    e_significant <- c(e_significant, effect)
+  } else {
+    e_significant <- c(e_significant, 0)
+  }
+
+  se_vals <- c(se_vals, summary(reg)$coefficients["treatedTRUE:post37TRUE","Std. Error"])
+  p_vals <- c(p_vals, summary(reg)$coefficients["treatedTRUE:post37TRUE","Pr(>|t|)"])
+}
+states_data <- data.frame(state=(subset_state_data_1925_1943_long %>%
+                        select(state) %>%
+                        distinct())$state)
+states_data$seperate_effects_all <- e_all
+states_data$seperate_effects_significant <- e_significant
+states_data$power_sample_size <- needed_sample
+states_data$se <- se_vals
+states_data$p_vals <- p_vals
+rownames(states_data) <- states_data$state
+
+# States Effect (Table A.1)
+states_effects <- states_data[c(
+  "seperate_effects_all",
+  "seperate_effects_significant",
+  "se",
+  "p_vals"
+)]
+writeLines(print(xtable(states_effects)), "table_did_weighted.tex")
+
+# dump csv for the heatmap (figure 3)
+write.csv(states_data, "heatmap_mmr.csv")
+
+## Table 6- Extreme State Effects
+extreme_states_effects <- states_effects[
+  c(
+    which.max(states_effects$seperate_effects_all),
+    which.min(states_effects$seperate_effects_all)
+  ),
+][c("seperate_effects_all")]
+print(xtable(extreme_states_effects))
+
+
+## Figure 4 - Black Population Ratio
+#The data is from census 1930 and attached in excel file. Heat maps produced using the website: http://www.heatmapper.ca/geomap/ 
+
+
+## Figure 5
+#from website: https://en.wikipedia.org/wiki/75th_United_States_Congress
 
 ### Sub section-Power Calculations
 
 ## Table A.4 in the appendix- Power Calculations
 power_table <- states_data[c("seperate_effects_all", "power_sample_size")]
 power_table <- power_table[order(power_table$power_sample_size), ]
-writeLines(xtable(power_table), "power.tex")
+writeLines(print(xtable(power_table)), "power.tex")
